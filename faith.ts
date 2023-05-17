457e662f1af5a7be64e3882d8c152a1ecb79f6b7
faith34 <Faithobiekezie345@gmail.com>
2023-05-17 15:06:10 +0100

    update and deleting in http server

--- faith.ts
+++ faith.ts
@@ -1,4 +1,7 @@
 import http, { IncomingMessage } from "http"
+import fs from "fs"
+import path from "path"
+
 
 
 interface IdataEntry{
@@ -10,7 +13,7 @@ interface IData{
     name:string,
     status:number,
     sucess:boolean,
-    data: IdataEntry[]|null
+    data: IdataEntry|IdataEntry[]|null
 }
 let dataEntry :IdataEntry[]=[
     {id:1, course:"node"},
@@ -21,9 +24,10 @@ let data :IData={
     name:"request error",
     status:404,
     sucess:false,
-    data:null
+       data:null
 }
 const port:number =3345;
+const dataPath=path.join(__dirname,"sample","data.json")
 const server : http.Server<typeof http.IncomingMessage,
 typeof http.ServerResponse>=http.createServer((
     req: http.IncomingMessage,
@@ -51,8 +55,55 @@ typeof http.ServerResponse>=http.createServer((
         // writing from static db
         else if(method == "POST" && url === "/"){
             dataEntry.push(JSON.parse(body));
-            
+        
+            fs.writeFile(dataPath, JSON.stringify(dataEntry), ()=>{
+
+            })
             data.message="Reading from database";
+            data.name="GET request";
+            data.status= 201;
+            data.sucess=true;
+            data.data= dataEntry;
+        }
+        // single from static db
+        else if(method ===  "GET"){
+            // dataEntry.push(JSON.parse(body));
+            let id=req.url?.split("/")[1];
+
+            // console.log(id);
+
+            data.message="Reading from database";
+            data.name="GET request";
+            data.status= 200;
+            data.sucess=true;
+            data.data= dataEntry[parseInt(id!) - 1];
+        }
+        // Deleting from static db
+        else if(method == "DELETE" && url === "/"){
+            // dataEntry.push(JSON.parse(body));
+            let id = parseInt(req.url?.split("/")[1]!) - 1;
+            
+
+            let value= dataEntry.filter((el:any)=>{
+                return el.id === id;
+            })
+            
+            data.message="Deleting from database";
+            data.name="GET request";
+            data.status= 201;
+            data.sucess=true;
+            data.data= value;
+        }
+         // updating from static db
+         else if(method == "PATCH"){
+            // dataEntry.push(JSON.parse(body));
+            const {course}= JSON.parse(body);
+            let id = parseInt(req.url?.split("/")[1]!) - 1;
+            
+
+            dataEntry[id].course = course
+            
+            data.message="Deleting from database";
             data.name="GET request";
             data.status= 201;
             data.sucess=true;
